83dce4cc1c277928c0743fd73239d17a2b7a9422
Badal1e <151656632+Badal1e@users.noreply.github.com>
2025-04-29 16:49:45 +0530

    new files are added

---
+++ Railway-booking-website-master/onclick.js
@@ -0,0 +1,22 @@
+function newtab1() {
+    window.open("index.html", "_self")
+};
+
+function newtab2() {
+    window.open("meals.html", "_self")
+};
+
+function newtab3() {
+    window.open("holiday packages.html", "_self")
+
+};
+
+function newtab4() {
+    window.open("service.html", "_self")
+
+};
+
+function newtab5() {
+
+    window.open("contact.html", "_self")
+};
